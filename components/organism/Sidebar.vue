eaeee11d586f1a0a8e4f672ae65fb49f8ae02af9
Lutfi <nofalpratamalutfi@google.com>
2025-01-24 10:49:39 +0700

    sidebar expand collapse

--- components/organism/Sidebar.vue
+++ components/organism/Sidebar.vue
@@ -5,6 +5,12 @@ const { data: dataSidebar } = await useFetch(
 
 const choose = ref("post");
 
+const expand = ref(false);
+
+const menuExpand = () => {
+  expand.value = !expand.value;
+};
+
 const handleClick = (id) => {
   choose.value = id;
 };
@@ -13,14 +19,18 @@ const handleClick = (id) => {
 <template>
   <div>
     <div
-      class="w-72 h-screen flex flex-col border-r font-sans max-sm:hidden"
+      :class="[
+        expand
+          ? 'w-72 h-screen flex flex-col border-r font-sans max-sm:hidden'
+          : 'w-42 h-screen flex flex-col border-r font-sans max-sm:hidden',
+      ]"
     >
       <img
         src="/img/logo.png"
         alt="logo-kompas"
         class="w-[32px] h-[32px] mx-10 my-8"
       />
-      <div class="flex flex-col gap-7">
+      <div v-if="expand" class="flex flex-col gap-7">
         <div v-for="menuSidebar in dataSidebar" :key="menuSidebar.id">
           <NuxtLink @click="handleClick(menuSidebar.id)" to="#">
             <div
@@ -60,6 +70,61 @@ const handleClick = (id) => {
             </div>
           </NuxtLink>
         </div>
+      </div>
+      <div v-else class="flex flex-col gap-7">
+        <div v-for="menuSidebar in dataSidebar" :key="menuSidebar.id">
+          <NuxtLink @click="handleClick(menuSidebar.id)" to="#">
+            <div
+              :class="[
+                menuSidebar.id == choose
+                  ? 'flex flex-row items-center gap-7'
+                  : 'text-gray-700',
+              ]"
+            >
+              <div
+                :class="[
+                  menuSidebar.id == choose
+                    ? 'h-5 w-[6px] bg-blue-800 rounded-e-md'
+                    : '',
+                ]"
+              ></div>
+              <div
+                :class="[
+                  menuSidebar.id == choose
+                    ? 'bg-blue-200 text-blue-800 p-1 rounded-md w-full mr-7 font-semibold'
+                    : 'text-gray-700 ml-9 font-light',
+                ]"
+              >
+                <div class="flex flex-row items-center gap-4">
+                  <img
+                    :src="
+                      menuSidebar.id === choose
+                        ? `https://cdn-content.kompas.id/icm/${menuSidebar.icon[1]}`
+                        : `https://cdn-content.kompas.id/icm/${menuSidebar.icon[0]}`
+                    "
+                    alt="icon"
+                    class="w-[20px] h-[20px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="px-10 py-24">
+        <button class="flex flex-row gap-3 items-center" v-if="expand" @click="menuExpand">
+          <Icon
+          name="material-symbols:arrow-back-rounded"
+          class="w-[20px] h-[20px] text-gray-700"
+          />
+          <div class="font-sans text-gray-700 text-base">Collapse</div>
+        </button>
+        <button v-else @click="menuExpand">
+          <Icon
+            name="material-symbols:arrow-forward-rounded"
+            class="w-[20px] h-[20px] text-gray-700"
+          />
+        </button>
       </div>
     </div>
   </div>
